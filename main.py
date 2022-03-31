78c8d15cd19c0a5b52ecdb4d131c7b7046d303e9
TyurinaElena <elena.tyurina.2002@gmail.com>
2022-04-01 02:05:28 +0300

    ДЗ и модуль с  играми

--- main.py
+++ main.py
@@ -48,7 +48,13 @@ def get_text_message(message):
         elif ms_text == "Прислать анекдот":
             bot.send_message(chat_id, text=get_anekdot())
 
-        elif ms_text == "Карту":
+        elif ms_text == "Прислать фильм":
+            send_film(chat_id)
+
+        elif ms_text == "Угадай, кто?":
+            get_ManOrNot(chat_id)
+
+        elif ms_text == "Карту!":
             if game21 == None:
                 goto_menu(chat_id, "Выход")
                 return
@@ -140,6 +146,20 @@ def send_help(chat_id):
     img = open('1МД15_Тюрина_Елена.jpg', 'rb')
     bot.send_photo(chat_id, img, reply_markup=markup)
 
+def send_film(chat_id):
+    film = get_randomFilm()
+    info_str = f"<b>{film['Наименование']}</b>\n" \
+               f"Год: {film['Год']}\n" \
+               f"Страна: {film['Страна']}</b>\n" \
+               f"Жанр: {film['Жанр']}\n" \
+               f"Продолжительность: {film['Продолжительность']}"
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text="Трейлер", url=film["Трейлер_url"])
+    btn2 = types.InlineKeyboardButton(text="Смотреть онлайн", url=film["фильм_url"])
+    markup.add(btn1, btn2)
+    bot.send_photo(chat_id, photo=film['Обложка_url'], caption=info_str, parse_mode='HTML',
+                   reply_markup=markup)
+
 def get_anekdot():
     array_anekdots = []
     req_anek = requests.get('http://anekdotme.ru/random')
@@ -160,6 +180,47 @@ def get_dogURL():
         r_json = req.json()
         url = r_json['url']
     return url
+
+def get_ManOrNot(chat_id):
+    global bot
+
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text="Проверить", url="https://vc.ru/dev/58543-thispersondoesnotexist-sayt-generator-realistichnyh-lic")
+    markup.add(btn1)
+
+    req = requests.get("https://thispersondoesnotexist.com/image", allow_redirects=True)
+    if req.status_code == 200:
+        img = BytesIO(req.content)
+        bot.send_photo(chat_id, photo=img, reply_markup=markup, caption="Этот человек реален?")
+
+def get_randomFilm():
+    url = 'https://randomfilm.ru/'
+    infoFilm = {}
+    req_film = requests.get(url)
+    soup = bs4.BeautifulSoup(req_film.text, "html.parser")
+    result_find = soup.find ('div', align="center", style="width: 100%")
+    infoFilm["Наименование"] = result_find.find("h2").getText()
+    names = infoFilm["Наименование"].split(" / ")
+    infoFilm["Наименование_rus"] = names[0].strip()
+    if len(names) > 1:
+        infoFilm["Наименование_eng"] = names[1].strip()
+
+    images = []
+    for img in result_find.findAll('img'):
+        images.append(url + img.get('src'))
+    infoFilm["Обложка_url"] = images[0]
+    details = result_find.findAll('td')
+    infoFilm["Год"] = details[0].contents[1].strip()
+    infoFilm["Страна"] = details[1].contents[1].strip()
+    infoFilm["Жанр"] = details[2].contents[1].strip()
+    infoFilm["Продолжительность"] = details[3].contents[1].strip()
+    infoFilm["Режиссёр"] = details[4].contents[1].strip()
+    infoFilm["Актёры"] = details[5].contents[1].strip()
+    infoFilm["Трейлер_url"] = url + details[6].contents[0]["href"]
+    infoFilm["фильм_url"] = url + details[7].contents[0]["href"]
+
+    return infoFilm
+
 
 def get_wolf_quote():
     array_quotes = []
